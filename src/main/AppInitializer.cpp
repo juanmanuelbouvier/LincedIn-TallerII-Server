454842b9a64c372af5f55db490664f0d25047a8c
Juan Manuel Bouvier <juanmanuel.bouvier@adoos.com>
2016-08-29 00:24:12 -0300

    Integrated GoogleTest and implemented DummyTest. Checking if travis runs the tests.

--- src/main/AppInitializer.cpp
+++ src/main/AppInitializer.cpp
@@ -7,9 +7,8 @@ using namespace std;
 * Launchs the application server.<BR>
 * Prints the app-server version.
 */
-int main()
+int main(int argc, char **argv)
 {
-    // TODO
     cout << LincedInServer_VERSION_MAJOR << "." << LincedInServer_VERSION_MINOR << endl;
     return 0;
 }
---
+++ src/test/test_main.cpp
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+#include "gtest/include/gtest/gtest.h"
+#include "DummyTests.cpp"	// TODO: include .h, NEVER .cpp!
+
+int main(int argc, char **argv) {
+  printf("Running main() from test_main.cpp");
+  testing::InitGoogleTest(&argc, argv);
+  return RUN_ALL_TESTS();
+}
