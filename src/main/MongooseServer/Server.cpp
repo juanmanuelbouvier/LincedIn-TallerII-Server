1b41512a4645fc86d7b5d0252fa0f297702806a2
tbert <tomasbert@hotmail.com>
2016-09-02 17:43:53 -0300

    Add files to compile

---
+++ src/main/MongooseServer/Server.cpp
@@ -0,0 +1,84 @@
+#include "Server.h"
+#include <map>
+
+volatile static bool SERVER_RUNNING;
+
+Server::Server(int connectionPort) {
+	mg_mgr_init(&eventManager, NULL);
+
+	SERVER_RUNNING = false;
+	port = to_string(connectionPort);
+
+	bindOptions.user_data = (void*)this;
+
+	mongooseConnection = mg_bind_opt(&eventManager, port.c_str(), this->eventHandler, bindOptions);
+	if (!mongooseConnection){
+		//TODO: Lanzar Excepcion, no se puede continuar.
+		Log("Failed to create Server in port: " + port + " [check that it is not running]", ERROR );
+		return;
+	} else {
+		settingUpConnection();
+	}
+}
+
+void Server::start(){
+	SERVER_RUNNING = true;
+
+	signal(SIGINT, this->stop);
+	Log("Server start at port: " + port + " [close it with Ctrl-C SIGINT]");
+
+	while (SERVER_RUNNING) {
+    	mg_mgr_poll(&eventManager, 1000);
+  	}
+}
+
+void Server::settingUpConnection(){
+	mg_set_protocol_http_websocket(mongooseConnection);
+	mg_enable_multithreading(mongooseConnection);
+}
+
+map<int, string> DEBUG_eventCodes = {
+		{MG_EV_ACCEPT, "MG_EV_ACCEPT"},
+		{MG_EV_CONNECT, "MG_EV_CONNECT"},
+		{MG_EV_RECV, "MG_EV_RECV"},
+		{MG_EV_SEND, "MG_EV_SEND"},
+		{MG_EV_POLL, "MG_EV_POLL"},
+		{MG_EV_TIMER, "MG_EV_TIMER"},
+		{MG_EV_CLOSE,"MG_EV_CLOSE"}
+};
+
+void Server::eventHandler(struct mg_connection* connection, int eventCode, void* eventData){
+	//Por el momento solo manejamos request del tipo http.
+
+	//TODO: Si trabajamos solo con HTTP hay que ver este link [https://docs.cesanta.com/mongoose/master/#/c-api/http_server.h/mg_register_http_endpoint.md/]
+	if (eventCode == MG_EV_HTTP_REQUEST){
+		struct http_message* msg = (struct http_message *)eventData;
+
+		string verb = string(msg->method.p,msg->method.len);
+		string uri = string(msg->uri.p,msg->uri.len);
+		string query = string(msg->query_string.p,msg->query_string.len);
+
+		string mensaje = "<h1>Linced In App Server</h1>\n<h2><b>METODO: </b>"+ verb +"</h2>\n<h2><b>URI: </b>"+ uri +"</h2>\n<h2><b>QUERY: </b>"+ query +"</h2>";
+
+		mg_send_head(connection, 200, mensaje.length(), "HTTP/1.1");
+		mg_printf(connection, "%s",mensaje.c_str());
+
+		connection->flags |= MG_F_SEND_AND_CLOSE;
+
+	} else if (eventCode != MG_EV_POLL) {
+		Log("EventHandler received " + DEBUG_eventCodes[eventCode] + " (eventCode: " + to_string(eventCode) + ") [see more: bit.ly/2bIuVuI]",WARNING);
+	}
+}
+
+void Server::stop(int signal){
+	if(!SERVER_RUNNING) return;
+	SERVER_RUNNING = false;
+	Log("Server close");
+}
+
+
+Server::~Server() {
+	if (SERVER_RUNNING) this->stop();
+	mg_mgr_free(&eventManager);
+}
+
---
+++ src/main/Util/Util.h
@@ -0,0 +1,11 @@
+#ifndef UTIL_H_
+#define UTIL_H_
+
+#include <iostream>
+
+using namespace std;
+
+const string getTimeWithFormat(string Format = "[%Y/%m/%d %X] ");
+
+
+#endif /* UTIL_H_ */
