83b00cbaf858914742977878945533b7c1dce110
tbert <tomasbert12@gmail.com>
2016-11-20 22:18:12 -0300

    Fix acces token when reader

--- src/main/src/service/Access/AccessToken.cpp
+++ src/main/src/service/Access/AccessToken.cpp
@@ -16,6 +16,14 @@ string jwt_get_body( jwt* JsonWebToken ){
 	return dataDecoded.substr(dataDecoded.find("}.") + 1);
 }
 
+Json::Value convertJWT2Json(jwt* jwt_token) {
+	//Separo porque viene siempre el header {"typ":"JWT","alg":"HS256"}.[NUESTRO JSON]
+	string bodyToken = jwt_get_body(jwt_token);
+	jwt_free(jwt_token);
+
+	return JSONUtils::stringToJSON(bodyToken);
+}
+
 Json::Value AccessToken::decode( string token ) {
 	const unsigned char* key_c = (unsigned char*)KEY;
 
@@ -23,20 +31,15 @@ Json::Value AccessToken::decode( string token ) {
 
 	int succes = jwt_decode(&JsonWebToken, token.c_str(),key_c, string(KEY).length());
 
-	if (succes != 0){
-		throw AccessException( Log("AccessToken.cpp::" + to_string(__LINE__) + ".Cannot Decode Json Web Token Object",ERROR) );
-	}
-
-	//Separo porque viene siempre el header {"typ":"JWT","alg":"HS256"}.[NUESTRO JSON]
-	string bodyToken = jwt_get_body(JsonWebToken);
-	jwt_free(JsonWebToken);
-
-	return JSONUtils::stringToJSON(bodyToken);
+	return (succes == 0) ? convertJWT2Json(JsonWebToken) :
+			throw AccessException( Log("AccessToken.cpp::" + to_string(__LINE__) + ".Cannot Decode Json Web Token Object",ERROR) );
 
 }
 
 string AccessToken::encode( Json::Value json ) {
 	jwt_t* JsonWebToken;
+	bool succes = false;
+	string token;
 	if ( jwt_new(&JsonWebToken) == 0 ) {
 		if ( jwt_set_alg(JsonWebToken, ALGORITHM, (unsigned char*)KEY, string(KEY).length()) == 0) {
 			Json::FastWriter writer;
@@ -44,15 +47,14 @@ string AccessToken::encode( Json::Value json ) {
 			string plainJSON = writer.write(json);
 			if ( jwt_add_grants_json(JsonWebToken,plainJSON.c_str()) == 0){
 				char* token_c = jwt_encode_str(JsonWebToken);
-				string token = string( token_c );
+				token = string( token_c );
 				jwt_free(JsonWebToken);
 				free(token_c);
-				return token;
+				succes = true;
 			}
 		}
 
 	}
-
-	throw AccessException( Log("AccessToken.cpp::" + to_string(__LINE__) + ".Cannot encode Json Value:\n\t" + json.toStyledString(),ERROR) );
+	return (succes) ? token : throw AccessException( Log("AccessToken.cpp::" + to_string(__LINE__) + ".Cannot encode Json Value:\n\t" + json.toStyledString(),ERROR) );
 }
 
