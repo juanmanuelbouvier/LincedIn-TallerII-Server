88fdd1f34caaac48803bb03e36dc75592b29aef0
tbert <tomasbert12@gmail.com>
2016-11-20 23:48:45 -0300

    Fix bug in DB. When DB cant open throw exception

--- src/test/src/DBTest.cpp
+++ src/test/src/DBTest.cpp
@@ -89,3 +89,41 @@ TEST(DBTest, wrongUseOfDB) {
 	SettingManager::deleteInstance();
 
 }
+
+TEST(DBTest, higgerKey) {
+	string file = "{\"db_folder\":\".temp-test/\"}";
+	string filename = ".temp-test/testdb.json";
+
+	CreateFile(file,filename);
+
+	SettingManager* setting = SettingManager::getInstance();
+	setting->readFile(filename);
+
+	DB db("higger");
+	Json::Value data = db.getHigherKeyValue(5);
+	EXPECT_TRUE(data.isMember("error"));
+	EXPECT_FALSE(db.Delete("A"));
+
+	db.open();
+	db.store("A","{\"1\":\"1\"}");
+	db.store("B","{\"4\":\"4\"}");
+	db.store("C","{\"5\":\"5\"}");
+	db.store("D","{\"3\":\"3\"}");
+	db.store("E","{\"2\":\"2\"}");
+	Json::Value data1 = db.getHigherKeyValue(0);
+	EXPECT_EQ(data1["2"],"2");
+
+	Json::Value data2 = db.getHigherKeyValue(1);
+	EXPECT_EQ(data2["3"],"3");
+
+	Json::Value data3 = db.getHigherKeyValue(4);
+	EXPECT_EQ(data3["1"],"1");
+
+	Json::Value data4 = db.getHigherKeyValue(535);
+	EXPECT_TRUE(data4.isMember("error"));
+
+	EXPECT_TRUE(db.Delete("A"));
+
+	SettingManager::deleteInstance();
+
+}
