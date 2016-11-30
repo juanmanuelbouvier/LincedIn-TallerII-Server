3471c57d5fea873d6a58d92788f33183238594ca
FacuEt <facurawson@gmail.com>
2016-11-30 18:20:27 -0300

    add log in handler

--- src/main/src/service/HTTP/HTTPRequestHandler.cpp
+++ src/main/src/service/HTTP/HTTPRequestHandler.cpp
@@ -64,6 +64,8 @@ HTTPRequestHandler::HTTPRequestHandler() {
 }
 
 HTTPResponse* HTTPRequestHandler::handle(HTTPRequest* http_request){
+	count ++;
+	cout << "Request nro" + to_string(count) + " " + http_request->getMethod() " Uri-> " + http_request->getURI() << endl;
 	string uri = http_request->getURI();
 
 	//Se supone que por decision del server esto ya tiene que venir verificado
